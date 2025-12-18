65729aac71563c2f7cae0c464e92dc8911181de1
Mario Vellandi <mvellandi@hotmail.com>
2025-12-18 14:10:14 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,9 @@ import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 
 const config = defineConfig({
   plugins: [
-    nitroV2Plugin(),
+    nitroV2Plugin({
+      compatibilityDate: '2025-12-18',
+    }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
